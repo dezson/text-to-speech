0faa13c951f9cc439f0142738a5665160ea98fc4
dezson <dezsonagy2@gmail.com>
2020-07-22 18:35:08 +0200

    GRPC server with proto utilization

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/exec"
+	"flag"
+	"fmt"
+	"net"
+
+	pb "github.com/dezson/text-to-speech/api"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
 func main() {
-	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	port := flag.Int("p", 8080, "Port to listen to")
+	flag.Parse()
+
+	logrus.Infof("Listening port: %d", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatalf("Could not listen to port %d: %v", *port, err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
+	err = s.Serve(lis)
+	if err != nil {
+		logrus.Fatalf("Could not serve %v", err)
 	}
 }
