20fcf75e51a6f199391898feca933d788b954378
dezson <dezsonagy2@gmail.com>
2020-07-22 19:24:40 +0200

    Client implementation

---
+++ say/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io/ioutil"
+	"log"
+
+	pb "github.com/dezson/text-to-speech/api"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	backend := flag.String("b", "localhost:8080", "address of the say backend")
+	output := flag.String("o", "output.wav", "Wav file where the output will be written")
+	flag.Parse()
+
+	conn, err := grpc.Dial(*backend)
+	if err != nil {
+		log.Fatalf("Could not connect to %s: %v", *backend, err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTextToSpeechClient(conn)
+	text := &pb.Text{Text: "hello there"}
+	res, err := client.Say(context.Background(), text)
+	if err != nil {
+		log.Fatalf("Could not say %s: %v", text.Text, err)
+	}
+
+	if err := ioutil.WriteFile(*output, res.Audio, 0666); err != nil {
+		log.Fatalf("Could not write to %s: %v", *output, err)
+	}
+}
