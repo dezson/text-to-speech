61bb50a085173ca0603b0ce86cafeee1ad63ebb3
dezson <dezsonagy2@gmail.com>
2020-07-22 19:26:11 +0200

    Added server.Say implementation

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os/exec"
 
 	pb "github.com/dezson/text-to-speech/api"
 	"github.com/sirupsen/logrus"
@@ -14,7 +16,25 @@ import (
 type server struct{}
 
 func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
-	return nil, fmt.Errorf("Not implemented")
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("Could not create file: %v", err)
+	}
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("Could not close %s file: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite failed: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("Could not read %s file: %v", f.Name(), err)
+	}
+
+	return &pb.Speech{Audio: data}, nil
 }
 
 func main() {
